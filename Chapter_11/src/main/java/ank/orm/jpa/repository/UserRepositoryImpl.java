dcda322f3994d47152a958688c05596a27effda0
haoak <haoak@bsfit.com.cn>
2018-05-16 17:24:46 +0800

    spring test
    chapter_13 finished
    chapter_14 start

---
+++ Chapter_11/src/main/java/ank/orm/jpa/repository/UserRepositoryImpl.java
@@ -0,0 +1,13 @@
+package ank.orm.jpa.repository;
+
+import ank.orm.jpa.domain.User;
+
+public class UserRepositoryImpl {
+    public User add(User user) {
+        return null;
+    }
+
+    public int update(User user) {
+        return 0;
+    }
+}
